67c96846b6f03cab7a2d96f40b534a1d878fd533
Fabrice <fabrice.kergaline.simon@gmail.com>
2020-04-01 04:48:11 +0200

    Ajout Script

---
+++ AnimationCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCamera : MonoBehaviour
+{
+
+    public void startAnimation(){
+    	GetComponent<Animator>().SetBool("boule", true);
+    }
+
+    public void endAnimation(){
+    	GetComponent<Animator>().SetBool("boule", false);
+    }
+}
