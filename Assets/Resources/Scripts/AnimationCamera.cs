ccf381a592d89005c432c33e788ef2b97397e1e9
Fabrice <fabrice.kergaline.simon@gmail.com>
2020-04-02 18:59:48 +0200

    Ajout Mouvement des NPCs

--- Assets/Resources/Scripts/AnimationCamera.cs
+++ Assets/Resources/Scripts/AnimationCamera.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class AnimationCamera : MonoBehaviour
 {
+		public Transform player;
+		public Transform NPC;
+
 
     public void startAnimation(){
     	GetComponent<Animator>().SetBool("boule", false);
         FindObjectOfType<AudioManager>().Play("Hello");
+        transform.LookAt(player.transform.position);
     }
 
     public void endAnimation(){
     	GetComponent<Animator>().SetBool("boule", true);
+    	transform.LookAt(NPC.transform.position) ;
     }
 }
