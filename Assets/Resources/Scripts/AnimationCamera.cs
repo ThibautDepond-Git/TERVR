20fbe19f5f0926f6a23ab00f4407939927646bb6
aa303554 <aarnaud6@hotmail.fr>
2020-04-02 18:29:13 +0200

    interaction camera

---
+++ Assets/Resources/Scripts/AnimationCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationCamera : MonoBehaviour
+{
+
+    public void startAnimation(){
+    	GetComponent<Animator>().SetBool("boule", false);
+        FindObjectOfType<AudioManager>().Play("Hello");
+    }
+
+    public void endAnimation(){
+    	GetComponent<Animator>().SetBool("boule", true);
+    }
+}
