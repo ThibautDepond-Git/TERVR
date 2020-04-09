1621199fbbb8f362b5a32ce16b02f0b451a349ff
ThibautDepond-Git <pro.thibautdepond@gmail.com>
2020-04-09 12:54:13 +0200

    implem menu

---
+++ Assets/Resources/Scripts/MenuScripts/StartScenario.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartScenario : MonoBehaviour
+{
+
+    public int scenarioId;
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+}
---
+++ Assets/Resources/Scripts/basicLookAt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basicLookAt : MonoBehaviour
+{
+    public Transform looked;
+    void Start()
+    {
+        transform.LookAt(looked.transform.position);
+    }
+}
