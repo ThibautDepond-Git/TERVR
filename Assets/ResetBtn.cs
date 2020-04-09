e9ae035dc33a1d0f415496386f5b1cb925bf3fd4
ThibautDepond-Git <pro.thibautdepond@gmail.com>
2020-04-09 11:54:59 +0200

    ajout d'un simple bouton reset

---
+++ Assets/ResetBtn.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBtn : MonoBehaviour
+{
+    public void ResetIsTriggered()
+    {
+        FindObjectOfType<AudioManager>().Play("Hello");
+    }
+}
