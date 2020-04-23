1a327e4088d54c97fc2ad013aae4fb9f120354c7
ThibautDepond-Git <pro.thibautdepond@gmail.com>
2020-04-23 08:15:33 +0200

    reorganisation des packet + animation layer

--- Assets/Resources/Scripts/AnimationCamera.cs
+++ Assets/Resources/Scripts/AnimationCamera.cs
@@ -12,10 +12,11 @@ public class AnimationCamera : MonoBehaviour
     	GetComponent<Animator>().SetBool("boule", false);
         FindObjectOfType<AudioManager>().Play("Hello");
         transform.LookAt(player.transform.position);
+        transform.SetPositionAndRotation(transform.position, new Quaternion(0, transform.rotation.y, 0, transform.rotation.w));
     }
 
     public void endAnimation(){
     	GetComponent<Animator>().SetBool("boule", true);
-    	transform.LookAt(NPC.transform.position) ;
+    	transform.LookAt(NPC.transform.position);
     }
 }
