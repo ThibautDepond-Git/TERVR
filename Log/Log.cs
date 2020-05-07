e0cf54df17a12d4c5fd530056121401dce5241fb
aa303554 <aarnaud6@hotmail.fr>
2020-05-07 13:08:52 +0200

    debut log

---
+++ Log/Log.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Log : MonoBehaviour
+{
+    //public
+    //public 
+
+    void MyPrint()
+    {
+        //Debug.Log("Hello");
+        //Debug.Log("Hello");
+    }
+}
