ed28aaf61364bc81b8399edf7b8ca4ba05a3a354
aa303554 <aarnaud6@hotmail.fr>
2020-05-08 13:09:08 +0200

    update log

---
+++ Assets/Resources/Scripts/Log.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Log : MonoBehaviour
+{
+    public Transform player;
+    public Transform NPC;
+
+    public void MyPrint()
+    {
+        Debug.Log(player.transform.position);
+        Debug.Log(NPC);
+    }
+}
